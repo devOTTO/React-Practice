b64cac7744bd39778bf0113b0475bbb6eccb3d40
devOTTO <yoongotto@gmail.com>
2019-07-22 17:06:43 +0900

    React testing

---
+++ react-enzyme-test/src/HookCounter.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import HookCounter from './HookCounter';
+import toJson from 'enzyme-to-json'
+
+describe('<HookCounter />', () => {
+  it('matches snapshot', () => {
+    const wrapper = mount(<HookCounter />);
+    expect(toJson(wrapper)).toMatchSnapshot()
+  });
+  it('increases', () => {
+    const wrapper = mount(<HookCounter />);
+    let plusButton = wrapper.findWhere(
+      node => node.type() === 'button' && node.text() === '+1'
+    );
+    plusButton.simulate('click');
+    plusButton.simulate('click');
+
+    const number = wrapper.find('h2');
+
+    expect(number.text()).toBe('2');
+  });
+  it('decreases', () => {
+    const wrapper = mount(<HookCounter />);
+    let decreaseButton = wrapper.findWhere(
+      node => node.type() === 'button' && node.text() === '-1'
+    );
+    decreaseButton.simulate('click');
+    decreaseButton.simulate('click');
+
+    const number = wrapper.find('h2');
+
+    expect(number.text()).toBe('-2');
+  });
+});
---
+++ test-js/sum.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => {
+    return a+b;
+}
+
+function sumOf(numbers) {
+    return numbers.reduce((acc, current) => acc + current, 0);
+  }
+
+  
+exports.sum = sum
+exports.sumOf = sumOf
